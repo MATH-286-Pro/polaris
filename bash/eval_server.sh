8902ac61ba60f1b9248d5a4da35f7b3d2db2178a
jiajun <jhu114514@gmail.com>
2026-05-21 16:48:45 +0200

    add: 评估 bash

---
+++ bash/eval_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Starting from the root of this repo. This will setup openpi and host a pi05 policy.
+cd third_party/openpi
+# GIT_LFS_SKIP_SMUDGE=1 uv sync
+# GIT_LFS_SKIP_SMUDGE=1 uv pip install -e .
+XLA_PYTHON_CLIENT_MEM_FRACTION=0.35 uv run scripts/serve_policy.py --port 8000 policy:checkpoint --policy.config pi05_droid_jointpos_polaris --policy.dir gs://openpi-assets/checkpoints/polaris/pi05_droid_jointpos_polaris
